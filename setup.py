20d300240b663aa836f0249e401bf91fe9ab5848
Dave McLain <dave.mclain@mapmyfitnessinc.com>
2014-01-17 14:36:10 -0600

    roll in mmf release info

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from distutils.core import setup
+import sys
+
+if 'sdist' in sys.argv:
+    import mmf_release_tools
+    version = mmf_release_tools.generate_release_version('0.1', __file__)
+    mmf_release_tools.write_release_version(version)
+else:
+    with open("RELEASE-VERSION", "r") as f:
+        version = f.readlines()[0].strip()
+
 setup(name='xmltodict',
       author='Martin Blech',
-      version='0.1',
+      version=version,
       py_modules=['xmltodict'],
       )
